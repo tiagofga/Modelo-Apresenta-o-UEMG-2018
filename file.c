8bd06754b1da1b7dc1a88f31ce2200bc2f30b40f
Tiago Alves de Oliveira <tiagofga@gmail.com>
2021-06-02 08:16:50 -0300

    Initial Overleaf Import

---
+++ file.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    FILE *arquivo;
+
+
+    return 0;
+}
---
+++ open.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    FILE *arquivo;
+    arquivo = fopen("teste.txt","w");    
+
+    return 0;
+}
